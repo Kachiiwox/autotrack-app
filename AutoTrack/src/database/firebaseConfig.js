da6ab4ffecde26868f5df0c92a7bb3f958b156cd
Kachiiwox <kachiiwox@gmail.com>
2026-06-30 23:18:55 +0100

    feat: setup Firebase JS SDK and project structure

---
+++ AutoTrack/src/database/firebaseConfig.js
@@ -0,0 +1,22 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQOLuTJ8tiXlQ2rf5jqW1ag-xy2fno3lc",
+  authDomain: "autotrack-app-a6dbc.firebaseapp.com",
+  projectId: "autotrack-app-a6dbc",
+  storageBucket: "autotrack-app-a6dbc.firebasestorage.app",
+  messagingSenderId: "644802121767",
+  appId: "1:644802121767:web:716b876869862aa420fe02",
+  measurementId: "G-REJZRK0EFV"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase services
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
